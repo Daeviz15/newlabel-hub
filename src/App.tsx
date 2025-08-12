041d8f91bff3c24dbe481211e9e24c533af47176
unknown <daeviz15felix@gmail.com>
2025-08-12 13:49:18 +0100

    made new change

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,6 @@ const App = () => (
           <Route path="/contact" element={<ContactPage />} />
           <Route path="/cart" element={<CartPage />} />
 
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
