9b7b396b6bf45a0ed8ea7ec613c8a8752665845e
Daeviz15 <daeviz15felix@gmail.com>
2025-11-18 11:42:41 +0100

    final fix

--- src/pages/Catalogue.tsx
+++ src/pages/Catalogue.tsx
@@ -1,6 +1,6 @@
 import { HomeHeader } from "@/components/home-header";
 import { supabase } from "@/integrations/supabase/client";
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import Footer from "@/components/Footer";
 import { useNavigate } from "react-router-dom";
 import Tab from "@/components/Tab";
